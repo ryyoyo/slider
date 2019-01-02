6271f019b635b033a32c38da3bc647802cde0498
ry <2834016348@qq.com>
2019-01-02 22:22:21 +0800

    新增两张图片

--- main.js
+++ main.js
@@ -1,88 +1,105 @@
- 
+let $btns = $('#btnWrapper>button')
+let $slides = $('#slides')
+let $imgs = $slides.children('img')
+let current = 0
 
-/*setTimeout(function(){
-	$('.imgs>img:nth-child(1)').removeClass('current').addClass('leave')
-		.one('transitionend',function(e){
-			$(e.currentTarget).removeClass('leave').addClass('enter')
-		})
-	$('.imgs>img:nth-child(2)').removeClass('enter').addClass('current')
-},1000)
+makeFakeSlides()
+$slides.css({transform: 'translateX(-400px)'})
+bindEvents()
+$(next).on('click',function(){
+	goToSlide(current + 1)
+})
+$(previours).on('click',function(){
+	goToSlide(current - 1)
+})
 
-setTimeout(function(){
-	$('.imgs>img:nth-child(2)').removeClass('current').addClass('leave')
-		.one('transitionend',function(e){
-			$(e.currentTarget).removeClass('leave').addClass('enter')
-		})
-	$('.imgs>img:nth-child(3)').removeClass('enter').addClass('current')
-},2000)
-
-setTimeout(function(){
-	$('.imgs>img:nth-child(3)').removeClass('current').addClass('leave')
-		.one('transitionend',function(e){
-			$(e.currentTarget).removeClass('leave').addClass('enter')
-		})
-	$('.imgs>img:nth-child(1)').removeClass('enter').addClass('current')
+//自动播放
+let timer = setInterval(function(){
+	goToSlide(current + 1)
 },3000)
 
-setTimeout(function(){
-	$('.imgs>img:nth-child(1)').removeClass('current').addClass('leave')
-		.one('transitionend',function(e){
-			$(e.currentTarget).removeClass('leave').addClass('enter')
-		})
-	$('.imgs>img:nth-child(2)').removeClass('enter').addClass('current')
-},4000)
-*/
+$('.container').on('mouseenter',function(){
+	window.clearInterval(timer)
+}).on('mouseleave',function(){
+	timer = setInterval(function(){
+		goToSlide(current + 1)
+	},3000)
+})
 
-let n = 1
-y()
-setInterval(()=>{
-	makeLeave(getImg(n))
-		.one('transitionend', function(e){
-			makeEnter($(e.currentTarget))
-		})
-	makeCurrent(getImg(n+1))
-	n += 1
-},3000)
+function bindEvents(){
+	$('#btnWrapper').on('click','button',function(e){
+		let $btn = $(e.currentTarget)
+		let index = $btn.index()
+		goToSlide(index)
+	})
 
 
-
-
-
-
-
-
-function getImg(n){
-	return $(`.imgs>img:nth-child(${x(n)})`)
+/*	$btns.eq(0).on('click',function(){
+		if(current == 2){
+			$slides.css({transform: 'translateX(-1600px)'})
+				.one('transitionend',function(){
+					$slides.hide()
+						.offset()
+					$slides.css({transform: 'translateX(-400px)'})
+						.show()
+				})
+		}else{
+			$slides.css({transform: 'translateX(-400px)'})
+		}
+		current = 0
+	})
+	$btns.eq(1).on('click',function(){
+		$slides.css({transform: 'translateX(-800px)'})
+		current = 1
+	})
+	$btns.eq(2).on('click',function(){
+		if(current == 0){
+			$slides.css({transform: 'translateX(0px)'})
+				.one('transitionend',function(){
+					$slides.hide()
+						.offset()
+					$slides.css({transform: 'translateX(-1200px)'})
+						.show()
+				})
+		}else{
+			$slides.css({transform: 'translateX(-1200px)'})
+		}
+		current = 2
+	})*/
 }
 
-function x(n){
-	if(n>3)
-	n = n%3
-	if(n === 0){
-		n = 3
+function goToSlide(index){
+	if(index > $imgs.length-1){
+		index = 0
+	}else if(index < 0){
+		index = $imgs.length-1
 	}
-	return n
+	//最后一张到第一张
+	if(current === $imgs.length-1 && index === 0){
+		$slides.css({transform: `translateX(${-($imgs.length+1)*400}px)`})
+			.one('transitionend',function(){
+				$slides.hide().offset()
+				$slides.css({transform: `translateX(${-(index+1)*400}px)`}).show()
+			})
+	//第一张到最后一张
+	}else if(current === 0 && index === $imgs.length-1){
+		$slides.css({transform: 'translateX(0px)'})
+			.one('transitionend',function(){
+				$slides.hide().offset()
+				$slides.css({transform: `translateX(${-(index+1)*400}px)`}).show()
+			})
+	}else{
+		$slides.css({transform: `translateX(${-(index+1)*400}px)`})
+	}
+	current = index
 }
 
-function y(){
-	n = 1
-	$(`.imgs>img:nth-child(${n})`).addClass('current')
-		.siblings().addClass('enter')
-	/* 等于上面
-	$('.imgs>img:nth-child(1)').addClass('current')
-	$('.imgs>img:nth-child(2)').addClass('enter')
-	$('.imgs>img:nth-child(3)').addClass('enter')*/
-}
 
 
-function makeCurrent($node){
-	return $node.removeClass('enter leave').addClass('current')
-}
+function makeFakeSlides(){
+	let $firstCopy = $imgs.eq(0).clone(true) //true 表示将其里面的子元素全部克隆
+	let $lastCopy = $imgs.eq($imgs.length-1).clone(true)
 
-function makeLeave($node){
-	return $node.removeClass('current enter').addClass('leave')
-}
-
-function makeEnter($node){
-	return $node.removeClass('leave current').addClass('enter')
+	$slides.append($firstCopy)
+	$slides.prepend($lastCopy)
 }
